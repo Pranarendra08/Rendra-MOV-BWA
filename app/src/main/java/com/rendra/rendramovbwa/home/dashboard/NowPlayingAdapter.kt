11bed866c9d208bc722fc632847da440b13381b5
Pranarendra08 <pranarendra@gmail.com>
2022-01-18 18:01:06 +0700

    update NowPlayingAdapter

--- app/src/main/java/com/rendra/rendramovbwa/home/dashboard/NowPlayingAdapter.kt
+++ app/src/main/java/com/rendra/rendramovbwa/home/dashboard/NowPlayingAdapter.kt
@@ -1,14 +1,22 @@
 package com.rendra.rendramovbwa.home.dashboard
 
+import android.content.Context
+import android.view.ContentInfo
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.rendra.rendramovbwa.R
 import com.rendra.rendramovbwa.model.Film
 import java.util.*
 
 class NowPlayingAdapter(private var data: List<Film>,
                         private val listener:(Film) -> Unit)
     : RecyclerView.Adapter<NowPlayingAdapter.ViewHolder>() {
+
+    lateinit var contextAdapter : Context
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -18,15 +26,31 @@ class NowPlayingAdapter(private var data: List<Film>,
     }
 
     override fun onBindViewHolder(holder: NowPlayingAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bindItem(data[position], listener, contextAdapter)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = data.size
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val tvTitle:TextView = view.findViewById(R.id.tv_kursi)
+        private val tvGenre:TextView = view.findViewById(R.id.tv_genre)
+        private val tvRate:TextView = view.findViewById(R.id.tv_rate)
 
+        private val tvImage:ImageView = view.findViewById(R.id.iv_poster_image)
+
+        fun bindItem(data:Film, listener: (Film) -> Unit, context: Context) {
+            tvTitle.setText(data.judul)
+            tvGenre.setText(data.genre)
+            tvRate.setText(data.rating)
+
+            Glide.with(context)
+                .load(data.poster)
+                .into(tvImage)
+
+            itemView.setOnClickListener {
+                listener(data)
+            }
+        }
     }
 
 }

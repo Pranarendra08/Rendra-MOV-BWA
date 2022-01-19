02405f425fa2f06fc168b1692ab731ea140550cb
Pranarendra08 <pranarendra@gmail.com>
2022-01-19 19:38:42 +0700

    update checkout

--- app/src/main/java/com/rendra/rendramovbwa/checkout/CheckoutSuccessActivity.kt
+++ app/src/main/java/com/rendra/rendramovbwa/checkout/CheckoutSuccessActivity.kt
@@ -1,10 +1,23 @@
 package com.rendra.rendramovbwa.checkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rendra.rendramovbwa.R
+import com.rendra.rendramovbwa.home.HomeActivity
+import kotlinx.android.synthetic.main.activity_checkout_success.*
 
 class CheckoutSuccessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout_success)
+
+        btn_home.setOnClickListener {
+            finishAffinity()
+
+            var intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }

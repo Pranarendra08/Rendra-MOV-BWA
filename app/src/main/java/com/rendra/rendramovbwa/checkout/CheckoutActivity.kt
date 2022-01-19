5bf3577168a51b91838ae0435b530e23a13c91e0
Pranarendra08 <pranarendra@gmail.com>
2022-01-19 18:48:01 +0700

    update CheckoutAdapter.kt

--- app/src/main/java/com/rendra/rendramovbwa/checkout/CheckoutActivity.kt
+++ app/src/main/java/com/rendra/rendramovbwa/checkout/CheckoutActivity.kt
@@ -4,19 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rendra.rendramovbwa.R
 import com.rendra.rendramovbwa.model.Checkout
-import com.rendra.rendramovbwa.model.Film
-import kotlinx.android.synthetic.main.activity_pilih_bangku.*
+import com.rendra.rendramovbwa.utils.Preferences
+import kotlinx.android.synthetic.main.activity_checkout.*
 
 class CheckoutActivity : AppCompatActivity() {
 
     private var dataList = ArrayList<Checkout>()
-    private var total: Int
+    private var total: Int = 0
+    private lateinit var preferences: Preferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkout)
+
+        preferences = Preferences(this)
+        dataList = intent.getSerializableExtra("data") as ArrayList<Checkout>
+
+        for (a in dataList.indices) {
+            total += dataList[a].harga!!.toInt()
+        }
+
+        dataList.add(Checkout("Total harus dibayar", total.toString()))
+
+        rv_checkout.layoutManager = LinearLayoutManager(this)
+        rv_checkout.adapter = CheckoutAdapter(dataList) {
+
+        }
     }
 
 }
--- app/src/main/java/com/rendra/rendramovbwa/checkout/PilihBangkuActivity.kt
+++ app/src/main/java/com/rendra/rendramovbwa/checkout/PilihBangkuActivity.kt
@@ -95,7 +95,7 @@ class PilihBangkuActivity : AppCompatActivity() {
         }
 
         btn_home.setOnClickListener {
-            var intent = Intent(this, CheckoutActivity::class.java).putExtra("data", data)
+            var intent = Intent(this, CheckoutActivity::class.java).putExtra("data", dataList)
             startActivity(intent)
         }
     }

c90417dacc22f3f73016167abd2f9d21f58577d2
Pranarendra08 <pranarendra@gmail.com>
2022-01-19 17:57:01 +0700

    update activity_checkout.xml

--- app/src/main/java/com/rendra/rendramovbwa/checkout/CheckoutActivity.kt
+++ app/src/main/java/com/rendra/rendramovbwa/checkout/CheckoutActivity.kt
@@ -11,102 +11,12 @@ import kotlinx.android.synthetic.main.activity_pilih_bangku.*
 
 class CheckoutActivity : AppCompatActivity() {
 
-    var statusA1: Boolean = false
-    var statusA2: Boolean = false
-    var statusA3: Boolean = false
-    var statusA4: Boolean = false
-    var total: Int = 0
-
     private var dataList = ArrayList<Checkout>()
+    private var total: Int
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pilih_bangku)
-
-        val data = intent.getParcelableExtra<Film>("data")
-        tv_kursi.text = data!!.judul
-
-        a1.setOnClickListener {
-            if (statusA1) {
-                a1.setImageResource(R.drawable.ic_rectangle_empty)
-                statusA1 = false
-                total -= 1
-                beliTiket(total)
-            } else {
-                a1.setImageResource(R.drawable.ic_rectangle_selected)
-                statusA1 = true
-                total += 1
-                beliTiket(total)
-
-                val data = Checkout("A1", "70000")
-                dataList.add(data)
-            }
-        }
-
-        a2.setOnClickListener {
-            if (statusA2) {
-                a2.setImageResource(R.drawable.ic_rectangle_empty)
-                statusA2 = false
-                total -= 1
-                beliTiket(total)
-            } else {
-                a2.setImageResource(R.drawable.ic_rectangle_selected)
-                statusA2 = true
-                total += 1
-                beliTiket(total)
-
-                val data = Checkout("A2", "70000")
-                dataList.add(data)
-            }
-        }
-
-        a3.setOnClickListener {
-            if (statusA3) {
-                a3.setImageResource(R.drawable.ic_rectangle_empty)
-                statusA3 = false
-                total -= 1
-                beliTiket(total)
-            } else {
-                a3.setImageResource(R.drawable.ic_rectangle_selected)
-                statusA3 = true
-                total += 1
-                beliTiket(total)
-
-                val data = Checkout("A3", "70000")
-                dataList.add(data)
-            }
-        }
-
-        a4.setOnClickListener {
-            if (statusA4) {
-                a4.setImageResource(R.drawable.ic_rectangle_empty)
-                statusA4 = false
-                total -= 1
-                beliTiket(total)
-            } else {
-                a4.setImageResource(R.drawable.ic_rectangle_selected)
-                statusA4 = true
-                total += 1
-                beliTiket(total)
-
-                val data = Checkout("A4", "70000")
-                dataList.add(data)
-            }
-        }
-
-        btn_home.setOnClickListener {
-            var intent = Intent(this, CheckoutActivity::class.java).putExtra("data", data)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_checkout)
     }
 
-    private fun beliTiket(total: Int) {
-        if (total == 0) {
-            btn_home.setText("Beli Tiket")
-            btn_home.visibility = View.INVISIBLE
-        } else {
-            btn_home.setText("Beli Tiket ("+total+")")
-            btn_home.visibility = View.VISIBLE
-        }
-    }
 }

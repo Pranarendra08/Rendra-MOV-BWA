81a012295fd193c392b9a649f44d8c1879ca1f4b
Pranarendra08 <pranarendra@gmail.com>
2022-01-25 18:16:59 +0700

    update notifikasi

--- app/src/main/java/com/rendra/rendramovbwa/checkout/CheckoutActivity.kt
+++ app/src/main/java/com/rendra/rendramovbwa/checkout/CheckoutActivity.kt
@@ -1,9 +1,16 @@
 package com.rendra.rendramovbwa.checkout
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.app.NotificationCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.rendra.rendramovbwa.R
 import com.rendra.rendramovbwa.model.Checkout
@@ -30,6 +37,8 @@ class CheckoutActivity : AppCompatActivity() {
         btn_tiket.setOnClickListener {
             var intent =  Intent(this, CheckoutSuccessActivity::class.java)
             startActivity(intent)
+
+            showNotif()
         }
 
         dataList.add(Checkout("Total harus dibayar", total.toString()))
@@ -38,6 +47,41 @@ class CheckoutActivity : AppCompatActivity() {
         rv_checkout.adapter = CheckoutAdapter(dataList) {
 
         }
+    }
+
+    private fun showNotif() {
+        val NOTIFICATION_CHANNERL_ID = "channel_bwa_notif"
+        val context = this.applicationContext
+        var notificationManager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channelName = "BWAMOV Notif Channel"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+
+            val mChannel = NotificationChannel(NOTIFICATION_CHANNERL_ID, channelName, importance)
+            notificationManager.createNotificationChannel(mChannel)
+        }
+
+        val mIntent = Intent(this, CheckoutSuccessActivity::class.java)
+        val bundle = Bundle()
+        bundle.putString("id", "id_film")
+        mIntent.putExtras(bundle)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, mIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val builder = NotificationCompat.Builder(this, NOTIFICATION_CHANNERL_ID)
+        builder.setContentIntent(pendingIntent)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.mipmap.ic_launcher))
+            .setTicker("notif bwa starting")
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000, 1000, 1000, 1000))
+            .setLights(Color.RED, 3000, 3000)
+            .setDefaults(Notification.DEFAULT_SOUND)
+            .setContentTitle("Pembayaran Telah di Terima")
+            .setContentText("bwamov")
+
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(115, builder.build())
     }
 
 }

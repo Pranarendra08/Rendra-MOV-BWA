fd416ffeb7e4be81ab769c0e0d673536fc4099fb
Pranarendra08 <pranarendra@gmail.com>
2022-02-01 15:35:41 +0700

    update photo edit profile

--- app/src/main/java/com/rendra/rendramovbwa/EditProfile.kt
+++ app/src/main/java/com/rendra/rendramovbwa/EditProfile.kt
@@ -2,12 +2,29 @@ package com.rendra.rendramovbwa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.rendra.rendramovbwa.utils.Preferences
+import kotlinx.android.synthetic.main.activity_edit_profile.*
 
 class EditProfile : AppCompatActivity() {
+
+    private lateinit var preferences: Preferences
+    private lateinit var mDatabase: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
 
-        //fitur blum dibuat
+        preferences = Preferences(this)
+
+        Glide.with(this)
+            .load(preferences.getValue("url"))
+            .apply(RequestOptions.circleCropTransform())
+            .into(iv_profile_picture)
+
+
     }
 }
